67d3f90bbceb4c281c0f33244d356f31079062e7
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2023-02-08 21:24:06 -0500

    added additional cards for grid. still finalizing spacing and card component

--- src/pages/Men.tsx
+++ src/pages/Men.tsx
@@ -1,9 +1,13 @@
-import { Box, Card, CardBody, Image, Stack, Heading, Text, Divider, CardFooter, ButtonGroup, Button } from '@chakra-ui/react'; 
+import { Wrap, Box, Card, CardBody, Image, Stack, Heading, Text, Divider, CardFooter, ButtonGroup, Button } from '@chakra-ui/react'; 
 import card1 from '../assets/card1.jpg'
 import card2 from '../assets/card2.jpg'
 import card3 from '../assets/card3.jpg'
 import card4 from '../assets/card4.jpg'
-
+import card5 from '../assets/card5.jpg'
+import card6 from '../assets/card6.jpg'
+import card7 from '../assets/card7.jpg'
+import card8 from '../assets/card8.jpg'
+import card9 from '../assets/card9.jpg'
 
 export default function Men() {
     return (
@@ -16,15 +20,17 @@ export default function Men() {
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, assumenda veritatis ullam quas autem provident enim ea id aliquid deleniti.</p>
                 </Box>
             </Box>
-            <Box h='80vh'
+            <Box minH='85vh'
                 backgroundColor='blackAlpha'
                 w='100%'
                 display='flex'
                 >
             
-            <Box
+            <Wrap
+                spacing='20px'
                 display="flex"
                 flexDirection="row"
+                mx='auto'
                 gap='3'
                 mt='5'
                 >
@@ -152,7 +158,162 @@ export default function Men() {
                         </CardFooter>
                     </Card>
                 </Box>
-            </Box>
+                <Box>
+                    <Card maxW='xs'>
+                        <CardBody>
+                            <Image
+                            boxSize='sm'
+                            src={card5}
+                            alt='Green Nike'
+                            borderRadius='md'
+                            />
+                            <Stack mt='3' spacing='2'>
+                            <Heading size='sm'>Sneaker 5</Heading>
+                            <Text fontSize='xs'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, harum?
+                            </Text>
+                            <Text color='yellow.500' fontSize='xl'>
+                                $165
+                            </Text>
+                            </Stack>
+                        </CardBody>
+                        <CardFooter mt='-5'>
+                            <ButtonGroup spacing='2'>
+                            <Button variant='solid' colorScheme='yellow' size='sm'>
+                                Buy now
+                            </Button>
+                            <Button variant='ghost' colorScheme='yellow' size='sm'>
+                                Add to cart
+                            </Button>
+                            </ButtonGroup>
+                        </CardFooter>
+                    </Card>
+                </Box>
+                <Box>
+                    <Card maxW='xs'>
+                        <CardBody>
+                            <Image
+                            boxSize='sm'
+                            src={card7}
+                            alt='Green Nike'
+                            borderRadius='md'
+                            />
+                            <Stack mt='3' spacing='2'>
+                            <Heading size='sm'>Sneaker 6</Heading>
+                            <Text fontSize='xs'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, harum?
+                            </Text>
+                            <Text color='yellow.500' fontSize='xl'>
+                                $165
+                            </Text>
+                            </Stack>
+                        </CardBody>
+                        <CardFooter mt='-5'>
+                            <ButtonGroup spacing='2'>
+                            <Button variant='solid' colorScheme='yellow' size='sm'>
+                                Buy now
+                            </Button>
+                            <Button variant='ghost' colorScheme='yellow' size='sm'>
+                                Add to cart
+                            </Button>
+                            </ButtonGroup>
+                        </CardFooter>
+                    </Card>
+                </Box>
+                <Box>
+                    <Card maxW='xs'>
+                        <CardBody>
+                            <Image
+                            boxSize='sm'
+                            src={card6}
+                            alt='Green Nike'
+                            borderRadius='md'
+                            />
+                            <Stack mt='3' spacing='2'>
+                            <Heading size='sm'>Sneaker 7</Heading>
+                            <Text fontSize='xs'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, harum?
+                            </Text>
+                            <Text color='yellow.500' fontSize='xl'>
+                                $165
+                            </Text>
+                            </Stack>
+                        </CardBody>
+                        <CardFooter mt='-5'>
+                            <ButtonGroup spacing='2'>
+                            <Button variant='solid' colorScheme='yellow' size='sm'>
+                                Buy now
+                            </Button>
+                            <Button variant='ghost' colorScheme='yellow' size='sm'>
+                                Add to cart
+                            </Button>
+                            </ButtonGroup>
+                        </CardFooter>
+                    </Card>
+                </Box>
+                <Box>
+                    <Card maxW='xs'>
+                        <CardBody>
+                            <Image
+                            boxSize='sm'
+                            src={card8}
+                            alt='Green Nike'
+                            borderRadius='md'
+                            />
+                            <Stack mt='3' spacing='2'>
+                            <Heading size='sm'>Sneaker 3</Heading>
+                            <Text fontSize='xs'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, harum?
+                            </Text>
+                            <Text color='yellow.500' fontSize='xl'>
+                                $165
+                            </Text>
+                            </Stack>
+                        </CardBody>
+                        <CardFooter mt='-5'>
+                            <ButtonGroup spacing='2'>
+                            <Button variant='solid' colorScheme='yellow' size='sm'>
+                                Buy now
+                            </Button>
+                            <Button variant='ghost' colorScheme='yellow' size='sm'>
+                                Add to cart
+                            </Button>
+                            </ButtonGroup>
+                        </CardFooter>
+                    </Card>
+                </Box>
+                <Box>
+                    <Card maxW='xs'>
+                        <CardBody>
+                            <Image
+                            boxSize='sm'
+                            src={card9}
+                            alt='Green Nike'
+                            borderRadius='md'
+                            />
+                            <Stack mt='3' spacing='2'>
+                            <Heading size='sm'>Sneaker 3</Heading>
+                            <Text fontSize='xs'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, harum?
+                            </Text>
+                            <Text color='yellow.500' fontSize='xl'>
+                                $165
+                            </Text>
+                            </Stack>
+                        </CardBody>
+                        <CardFooter mt='-5'>
+                            <ButtonGroup spacing='2'>
+                            <Button variant='solid' colorScheme='yellow' size='sm'>
+                                Buy now
+                            </Button>
+                            <Button variant='ghost' colorScheme='yellow' size='sm'>
+                                Add to cart
+                            </Button>
+                            </ButtonGroup>
+                        </CardFooter>
+                    </Card>
+                </Box>
+            </Wrap>
             </Box>
         </>
     )
